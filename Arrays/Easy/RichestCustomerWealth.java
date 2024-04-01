a2f6a27bbed646f061714d5b5bd755c8cf147009
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-01 15:48:18 +0530

    Create RichestCustomerWealth.java

---
+++ Arrays/Easy/RichestCustomerWealth.java
@@ -0,0 +1,20 @@
+package Arrays.Easy;
+
+public class RichestCustomerWealth {
+    static int maximumWealth(int[][] arr) {
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int maxSum = 0;
+            for (int j = 0; j < arr[0].length; j++) {
+                maxSum += arr[i][j];
+            }
+            res = Math.max(res, maxSum);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{1,2},{3,4},{5,21},{6,3}};   
+        System.out.println(maximumWealth(arr));
+    }
+}
